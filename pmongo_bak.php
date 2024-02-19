06be45af62da1131aed6391d84365d08998b0c8c
贺春旸 <chunyang_he@139.com>
2024-02-19 15:41:29 +0800

    Add files via upload

--- pmongo_bak.php
+++ pmongo_bak.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Percona MongoDB3.4 HotBackup PHP Script 
+ * Percona MongoDB 5.0 HotBackup PHP Script 
  * https://github.com/hcymysql/PMongo-HotBackup
  *
  * UPDATE:
@@ -15,15 +15,20 @@
  */
  
 ini_set('date.timezone','Asia/Shanghai');
-//error_reporting(7);
 
-//*************修改下面的配置信息***************//
-$user = "admin"; //使用root用户权限
-$pwd = '123456'; 
-$host = '192.168.180.26'; //在从库上热备
-$port = '27017';
-$authdb = 'admin'; //权限认证数据库
-$BAKDIR = "/data/bak/";
+// 解析命令行参数
+$options = getopt("u:p:h:P:a:d:");
+
+if (!isset($options['u']) || !isset($options['p']) || !isset($options['h']) || !isset($options['P']) || !isset($options['a']) || !isset($options['d'])) {
+    die("Usage: php pmongo_bak.php -u <user> -p <password> -h <host> -P <port> -a <authdb> -d <bakdir>\n");
+}
+
+$user = $options['u'];
+$pwd = $options['p'];
+$host = $options['h'];
+$port = $options['P'];
+$authdb = $options['a'];
+$BAKDIR = $options['d'];
 $BAKDIR .= date('Y-m-d_H-i-s')."_bak";
 
 
@@ -33,28 +38,29 @@ $m = new MongoBak($user,$pwd,$host,$port,$authdb,$BAKDIR);
 Class MongoBak{
 
     function __construct(){ 
-	     $a = func_get_args();
-            $f = 'backup';
-            call_user_func_array(array($this,$f),$a); 
+	    $a = func_get_args();
+        $f = 'backup';
+        call_user_func_array(array($this,$f),$a); 
     }
 
     function backup($p1,$p2,$p3,$p4,$p5,$p6){
-           $this->mkdirs("{$p6}");
+        $this->mkdirs("{$p6}");
 	    $mongo = new MongoClient("mongodb://{$p1}:{$p2}@{$p3}:{$p4}/{$p5}");
 	    $mongo->setReadPreference(MongoClient::RP_SECONDARY); //如果你想在主库上备份，把这行代码注销掉即可，并把脚本部署在主库上。
-           $db = $mongo->admin;
+        $db = $mongo->admin;
 	    echo "\n".date('Y-m-d H:i:s')."    backup is running......\n";
-           $r = $db->command(
-  	          array(
+        $r = $db->command(
+  	              array(
           	       'createBackup' => 1,
-                    'backupDir' => $p6),
+                    'backupDir' => $p6
+                  ),
      	          array('timeout' => -1)    
 	    );
 
 	//print_r($r); //调试备份错误日志使用，如备份失败，打开注释。
 
 	    if($r['ok'] == 1){
-  	           echo date('Y-m-d H:i:s')."    backup is success.\n";
+  	        echo date('Y-m-d H:i:s')."    backup is success.\n";
 	    } else{
       	    echo date('Y-m-d_H:i:s')."    backup is failure.\n";
 	    }	
